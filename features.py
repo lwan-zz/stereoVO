299444df4d3d06044634529d7804e2d9d5d0e998
lwan <wanlogan@gmail.com>
2016-12-04 11:53:48 -0500

    correspondences working, descriptor detection working, but needs 3x3 grid

---
+++ features.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+import operator
+import collections
+
+def getFeatures(img, sizeWindowArray ):
+    
+    height, width= img.shape
+
+    xstep = int(width/sizeWindowArray[1])
+    ystep = int(height/sizeWindowArray[0])
+    arrayidx = 0
+    sift = cv2.xfeatures2d.SIFT_create(100)
+    windowidx = 0
+    
+#    desc = np.zeros(1)
+    kp = []
+    # Loop through windows
+    for y in range(0,ystep*sizeWindowArray[0],ystep):
+
+        for x in range(0,xstep*sizeWindowArray[1],xstep):
+            
+            # Find SIFT features
+            kpTemp, desctemp = sift.detectAndCompute(img[y:y+ystep-1,x:x+xstep-1],None)
+            
+            for idx in range(len(kpTemp)):
+                # Compensate for global offset
+                kpTemp[idx].pt = tuple(map(operator.add,  kpTemp[idx].pt, (x,y)))
+
+            if x==0 and y==0:
+                desc = desctemp
+            else:
+                desc = np.concatenate((desc,desctemp), axis=0)
+
+            kp = kp + kpTemp
+    
+    returns = (kp,desc)
+    return returns
+
+
+
+def getCorres(desc1, desc2):
+
+    k = int(2)
+    ratio = 0.75
+
+# Brute force with ratio test
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(desc1, desc2, k)
+    
+    good = []
+    for m,n in matches:
+        if m.distance < ratio*n.distance:
+            good.append([m])
+
+    return good
+
+
+
+
+
+
+
+
+
+
+
--- testfeature.py
+++ testfeature.py
@@ -1,13 +1,24 @@
-import featureDetect
+import features
 import cv2
 
-img = cv2.imread('/home/lwan/dev/dataset/2011_09_26_drive_0029_sync/image_02/data/0000000000.png',0)
+img1 = cv2.imread('/home/lwan/dev/dataset/2011_09_26_drive_0029_sync/image_02/data/0000000000.png',0)
+img2 = cv2.imread('/home/lwan/dev/dataset/2011_09_26_drive_0029_sync/image_03/data/0000000000.png',0)
+
 imgout = cv2.imread('/home/lwan/dev/dataset/2011_09_26_drive_0029_sync/image_02/data/0000000000.png')
 
-size = [4,4]
+size = [3,3]
 
-kp = featureDetect.getFeatures(img,size)
+kp1,desc1 = features.getFeatures(img1,size)
+kp2,desc2 = features.getFeatures(img2,size)
 
-kpimg = cv2.drawKeypoints(img,kp,imgout)
-cv2.imshow("Display Window",kpimg)
+corres = features.getCorres(desc1,desc2)
+
+img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,corres,imgout,flags=2)
+cv2.imshow("Display",img3)
 cv2.waitKey(0)
+
+# View features
+import pdb;pdb.set_trace()
+#kpimg = cv2.drawKeypoints(img1,kp1,imgout)
+#cv2.imshow("Display Window",kpimg)
+#cv2.waitKey(0)
